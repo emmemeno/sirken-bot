f49a2c63ab3f8f30f48849f6cddbd619e7b88f65
Marco <pompelmo@gmail.com>
2019-02-27 23:16:59 +0100

    added days on countdown function

--- sirken-bot.py
+++ sirken-bot.py
@@ -108,8 +108,13 @@ class Message_Composer:
         output = ""
         date_diff = d_from - d_to
         seconds_diff = date_diff.total_seconds()
+        days = int((seconds_diff // (60 * 60 * 24)))
         hours = int(seconds_diff // (60 * 60))
         minutes = int((seconds_diff // 60) % 60)
+        if days == 1:
+            output += "1 day "
+        if hours > 1:
+            output += str(hours) + " days "
         if hours == 1:
             output += "1 hour "
         if hours > 1:
